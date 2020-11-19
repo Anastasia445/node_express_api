d1a9b0994dfdbbdc9ec807ce1cde3ef8146498d6
WhiteWolf6 <54216526+WhiteWolf6@users.noreply.github.com>
2020-11-19 13:19:18 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
 import express from 'express';
+import mysql from './node_modules/mysql2/index.js';
 import bodyParser from 'body-parser';
-import departmentsRoutes from './routes/departments.js'
+/*import departmentsRoutes from './routes/departments.js'
 import managersRoutes from './routes/managers.js'
-import employeesRoutes from './routes/employees.js'
+import employeesRoutes from './routes/employees.js'*/
 
 const app = express();
 const PORT = 5000;
+ 
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "managers",
+  password: "password"
+})
+app.disable('etag');
+app.get("/api/getemployees", (req, res) => {
+    connection.query("SELECT * FROM employees", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/api/getmanagers", (req, res) => {
+    connection.query("SELECT * FROM managers", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/api/getdepartments", (req, res) => {
+    connection.query("SELECT * FROM departments", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/api/getemployees/:idManager", (req, res) => {
+    const idManager = req.params.idManager;
+    connection.query(`SELECT * FROM employees WHERE idManager=?`, [idManager], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/api/getmanagers/:idDepartment", (req, res) => {
+    const idDepartment = req.params.idDepartment;
+    connection.query(`SELECT * FROM managers WHERE idDepartment=?`, idDepartment, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
 
 app.use(bodyParser.json());
 
-app.use('/api/getdepartments', departmentsRoutes);
-app.use('/api/getemployees', employeesRoutes);
-app.use('/api/getmanagers', managersRoutes);
-
-//app.get('/', (req, res) =>res.send('sdfsd'));
-
+//app.use('/api/getdepartments', departmentsRoutes);
+//app.use('/api/getemployees', employeesRoutes);
+//app.use('/api/getmanagers', managersRoutes);
 
 app.listen(PORT, ()=> console.log(`Server running on port: http://localhost:${PORT}`))
